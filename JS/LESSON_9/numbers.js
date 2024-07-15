fbc662e20b850cdf67d14caa5f8a549ef9281bca
takaevramzan32 <takaevramzan32@gmail.com>
2024-07-15 14:27:26 +0300

    added new str

--- JS/LESSON_9/numbers.js
+++ JS/LESSON_9/numbers.js
@@ -1,4 +1,4 @@
-/твой возраст
+//твой возраст
 let myAge = 23;
 
 //год рождения
@@ -23,10 +23,40 @@ let costToStudy = 10000;
 let currentYear = 2024;
 
 //курс доллара
-let dollarToRub = 87,74;
+let dollarToRub = 87.74;
 
 //курс евро
-let euroToRub = 95,76;
+let euroToRub = 95.76;
 
 //курс биткоина
 let bitcoinToRub = 5508126;
+
+//текущий год минус год рождения
+let myAge2 = currentYear - birthYear;
+
+//текущий год минус твой возраст
+let birthYear2 = currentYear - myAge;
+
+//количество родных братьев плюс количество родных сестер
+let brothersAndSisters = numOfBrothers + numOfSisters;
+
+//курс евро умноженный на 1000
+let euro1000 = euroToRub * 1000;
+
+//курс доллара умноженный на 2.5
+let quaterEagleToRub = dollarToRub * 2.5;
+
+//курс биткоина разделенный на 10000
+
+
+//стоимость проезда до места обучения разделенная на курс доллара
+
+
+//количество человек в семье минус количество братьев минус количество сестер
+
+
+//0 деленный на твой возраст
+
+
+//35 минус твой возраст
+
