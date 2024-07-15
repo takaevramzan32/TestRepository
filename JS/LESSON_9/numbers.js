f4bb7b9f72bdedd535a01d698c8ec302545323c6
takaevramzan32 <takaevramzan32@gmail.com>
2024-07-15 14:04:14 +0300

    added js files

---
+++ JS/LESSON_9/numbers.js
@@ -0,0 +1,32 @@
+/твой возраст
+let myAge = 23;
+
+//год рождения
+let birthYear = 2001;
+
+//число рождения (без нолика в начале)
+let birthDay = 22;
+
+//количество родных братьев
+let numOfBrothers = 1;
+
+//количество родных сестер
+let numOfSisters = 3;
+
+//количество человек в семье
+let numOfFamilyMembers = 9;
+
+//стоимость проезда до места обучения
+let costToStudy = 10000;
+
+//текущий год
+let currentYear = 2024;
+
+//курс доллара
+let dollarToRub = 87,74;
+
+//курс евро
+let euroToRub = 95,76;
+
+//курс биткоина
+let bitcoinToRub = 5508126;
---
+++ JS/LESSON_9/wrongs.js
@@ -0,0 +1,9 @@
+let 1a;
+
+let wrong-name;
+
+let wrong~name;
+
+let 1-wrong;
+
+let let;
