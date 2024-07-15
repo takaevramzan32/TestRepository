16f255dcc96f9e1befa82a6078bc5013145cda04
takaevramzan32 <takaevramzan32@gmail.com>
2024-07-15 21:35:19 +0300

    Выполнено ещё несколько заданий

---
+++ JS/LESSON_9/concat.js
@@ -0,0 +1,5 @@
+let firstVariable = 'Данная строка -';
+let secondVariable = ' результат конкатенации';
+let thirdVariable = ' трех переменных.';
+
+console.log(firstVariable + secondVariable + thirdVariable); //Вывод в консоль: "Данная строка - результат конкатенации трех перемнных."
